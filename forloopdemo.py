f3edc901aa1c957d22d09429a2df79ef90be29d6
Akshaypatil211 <65766571+Akshaypatil211@users.noreply.github.com>
2026-07-30 09:49:35 +0000

    addingnewfiles

---
+++ forloopdemo.py
@@ -0,0 +1,34 @@
+#for loop demo
+#example
+fruits=["Apple","Banana","Orange"]
+for i in fruits:
+    print(i);
+
+for index,fruit in  enumerate(fruits):
+    print(f"{index}:{fruit}")
+print(fruits[2])
+
+#example 2
+total=0
+
+for num in range(1,6):
+    total+=num
+print(f"Sum:{total}")
+#------------------------------------------------------------------------
+#while loop
+
+count=1
+while count <=5:
+    print(count)
+    count+=1;
+
+#break-------------------------------------------------------------------------
+
+total=0
+print("break test")
+for num in range(1,11):
+    total+=num
+    if total >15:
+        print(f"num:{num}")
+        break;
+print(f"total:{total}")
